ab64baa5ab5b1016b116d584c2e8b8d348c11cb2
firozmnadaf <142077079+firozmnadaf@users.noreply.github.com>
2023-10-04 16:12:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function validateForm() {
+    var emailInput = document.getElementById("emailInput").value;
+    var passwordInput = document.getElementById("passwordInput").value;
+    var loginButton = document.getElementById("loginButton");
+
+    if (emailInput.trim() !== "" && passwordInput.trim() !== "") {
+        loginButton.classList.remove("redButton");
+        loginButton.classList.add("greenButton");
+        loginButton.disabled = false;
+    } else {
+        loginButton.classList.remove("greenButton");
+        loginButton.classList.add("redButton");
+        loginButton.disabled = true;
+    }
+}
